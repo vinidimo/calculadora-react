d1c84aff04b6c04613e1b26ffe790a4b7ba4fb58
Vinicius Dimo <vinidimo@gmail.com>
2023-03-05 11:48:17 -0300

    5 mar. 2023

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Calculadora from './main/Calculadora';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <div>
+      <h1>Calculadora</h1>
+      <Calculadora />
+    </div>
   </React.StrictMode>
 );
 
